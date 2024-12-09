0aa18363bb9dcadef80e9a4752600952db3b8d6e
SajithDev <sdevared@nd.edu>
2024-12-08 19:46:43 -0500

    Table created

--- traceTM_Sajith.py
+++ traceTM_Sajith.py
@@ -2,7 +2,6 @@ import csv
 import sys
 import os
 
-
 def ntm_bfs(ntm, tape, depth_limit):
     configs = [[["", ntm['start'], tape]]]
     level, index, end, end_type = 0, 0, False, ''
@@ -48,7 +47,7 @@ def ntm_bfs(ntm, tape, depth_limit):
             level += 1
             index = 0
             if level > depth_limit:
-                end_type = 'limit'
+                end_type = 'timed_out'
                 end = True
 
     return configs, end_type
@@ -73,48 +72,75 @@ def simulate_ntm(tape, file_name, depth_limit):
     
     configs, end_type = ntm_bfs(ntm, tape, depth_limit)
     
-    return configs, ntm, end_type 
-
+    return configs, ntm, end_type
 
 def output(configs, ntm, tape, end_type, output_file):
     output_file.write(f"Machine Name: {ntm['Name']}\n")
-    output_file.write(f"Initial String: {tape}\n")
-    output_file.write(f"Tree Depth: {len(configs) - 1}\n")
-    output_file.write(f"Total Transitions Simulated: {sum([len(x) for x in configs])}\n")
-    output_file.write(f"Measured nondeterminism: {sum([len(x) for x in configs if x]) / len([x for x in configs if x])}\n")
+    output_file.write(f"Input String: {tape}\n")
+    output_file.write(f"Result: {end_type}\n")
+    output_file.write(f"Depth: {len(configs) - 1}\n")
+    output_file.write(f"Total # Configurations Simulated: {sum([len(x) for x in configs])}\n")
+    output_file.write(f"Average Non-Determinism: {sum([len(x) for x in configs if x]) / len([x for x in configs if x])}\n")
     
     if end_type == 'accepted':
         output_file.write(f"String accepted in {len(configs) - 1}\n")
     elif end_type == 'rejected':
         output_file.write(f"String rejected in {len(configs) - 1}\n")
-    elif end_type == 'limit':
+    elif end_type == 'timed_out':
         output_file.write(f"Execution stopped after {len(configs)}\n")
     
     for level in configs:
         output_file.write(' '.join(''.join(c) for c in level) + '\n')
 
-
+def output_to_csv(configs, ntm, tape, end_type, csv_writer):
+    total_configurations = sum([len(x) for x in configs])
+    non_determinism = sum([len(x) for x in configs if x]) / len([x for x in configs if x]) if configs else 0
+    
+    csv_writer.writerow([
+        ntm['Name'],
+        tape,
+        end_type,
+        len(configs) - 1,
+        total_configurations,
+        non_determinism
+    ])
 
 def main():
-    prev_file = "" # Used to keep track of count of test for each machine
-    with open('test_cases.txt', 'r') as test_file: # Reads all the test cases and runs the NTM for each test case
-        for line in test_file:
-            tape, depth_limit, file_name = line.strip().split()
-            if file_name.strip() != prev_file:
-                count = 1
-                prev_file = file_name.strip()
+    prev_file = ""  # Used to keep track of count of test for each machine
+    with open('test_cases.txt', 'r') as test_file:  # Reads all the test cases and runs the NTM for each test case
+        with open('test_case_results.csv', mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file) 
+            csv_writer.writerow([
+                'Machine Name',
+                'Input String',
+                'Result',
+                'Depth',
+                'Total # Configurations Simulated',
+                'Average Non-Determinism'
+            ])
 
-            input_file_path = file_name
+            count = 1
+            for line in test_file:
+                tape, depth_limit, file_name = line.strip().split()
+                if file_name.strip() != prev_file:
+                    count = 1
+                    prev_file = file_name.strip()
+
+                input_file_path = file_name
         
-            configs, ntm, end_type = simulate_ntm(tape, input_file_path, int(depth_limit))
+                configs, ntm, end_type = simulate_ntm(tape, input_file_path, int(depth_limit))
 
-            num_machine = file_name.split('_')[1]
-            output_file_path = os.path.join('outputs', f'output_machine_{num_machine}_test-{count}_Sajith.txt')
+                # Detailed output for current test case
+                num_machine = file_name.split('_')[1]
+                output_file_path = os.path.join('outputs', f'output_machine_{num_machine}_test-{count}_Sajith.txt')
 
-            # Output to corresponding file for the current test case
-            with open(output_file_path, 'w') as output_file:
-                output(configs, ntm, tape, end_type, output_file)
+                with open(output_file_path, 'w') as output_file:
+                    output(configs, ntm, tape, end_type, output_file)
 
-            count += 1
+                # Output to CSV to create table of NTM results
+                output_to_csv(configs, ntm, tape, end_type, csv_writer)
+
+                count += 1
+
 if __name__ == '__main__':
     main()
